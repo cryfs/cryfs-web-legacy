b10116fc123c2b037e783182847d73c360d2819a
Sebastian Messmer <mail@smessmer.de>
2015-05-02 18:21:17 +0200

    Send a notification mail to me when a new user enlists

--- app/controllers/interested_users_controller.rb
+++ app/controllers/interested_users_controller.rb
@@ -4,6 +4,7 @@ class InterestedUsersController < ApplicationController
     @interested_user = InterestedUser.new(interested_user_params)
     respond_to do |format|
       if @interested_user.save
+        send_notification_mail_to_me()
         #TODO Make site work without javascript -> form will then redirect here and call this html action, we have to redirect it back to the main page
         #format.html { render json: @interested_user }
         #TODO Only render subfields of @interested_user (via jsbuilder?)
@@ -18,5 +19,9 @@ class InterestedUsersController < ApplicationController
 private
   def interested_user_params
     params.require(:interested_user).permit(:email)
+  end
+
+  def send_notification_mail_to_me
+    ActionMailer::Base.mail(:from => "noreply@cryfs.org", :to => "mail@smessmer.de", :subject => "New interested user enlisted", :body => "A new interested user enlisted on cryfs.org:\n#{@interested_user.email}\n").deliver
   end
 end
