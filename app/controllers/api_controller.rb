63360ac4006586d1e18de04864b754ca2be604c9
Sebastian Messmer <mail@smessmer.de>
2015-11-24 09:00:42 +0100

    Bump CryFS version to 0.8.3

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   def version_info
     respond_to do |format|
-      response = {version_info: {current: "0.8.2"}, warnings: {"0.8": "This version is highly unstable. Please update!"}}
+      response = {version_info: {current: "0.8.3"}, warnings: {}}
       format.json { render json: response, status: :ok }
     end
   end
