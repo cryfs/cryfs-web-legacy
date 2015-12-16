08e6c4c155c742fd45a56eeebe3cbdb81e88ee96
Sebastian Messmer <mail@smessmer.de>
2015-12-16 04:37:52 +0100

    Update cryfs release version number in API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   def version_info
     respond_to do |format|
-      response = {version_info: {current: "0.8.3"}, warnings: {}}
+      response = {version_info: {current: "0.8.4"}, warnings: {}}
       format.json { render json: response, status: :ok }
     end
   end
