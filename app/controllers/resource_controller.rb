0cfc51eb8e64d8a702e045b0bc98c9ae676f8fbb
Sebastian Messmer <mail@smessmer.de>
2015-12-18 18:48:18 +0100

    Push requests to install.sh to analytics

---
+++ app/controllers/resource_controller.rb
@@ -0,0 +1,10 @@
+class ResourceController < ApplicationController
+  include GoogleAnalytics
+
+  def install_script
+    respond_to do |format|
+      format.sh { render file: 'resource/install.sh'}
+      trackPageview('Download Install Script')
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get '/legal_notice' => 'main#legal_notice'
   get '/version_info' => 'api#version_info'
+  get '/install' => 'resource#install_script'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
